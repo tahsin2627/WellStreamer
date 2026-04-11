03692032936a74bedb0023285230e5532de042b9
tahsin2627 <tahsinhassan280@gmail.com>
2026-04-11 11:21:54 +0600

    Update LoginPage.jsx

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import { useAuth } from '../lib/auth.js'
+import { useAuth } from '../lib/auth.jsx'
 
 export default function LoginPage() {
   const { login, register } = useAuth()
